8f4ce53c23369ebf20f242ba4498b630a01b654a
taehyun_0222 <taehyun0222@naver.com>
2026-01-16 08:27:59 +0900

    기본 멤버변수 추가 및 초기화 + 생성자, 소멸자 출력

--- CMyStringProject/CMyStringProject/CMyString.h
+++ CMyStringProject/CMyStringProject/CMyString.h
@@ -1,12 +1,17 @@
 #pragma once
+#include <iostream>
+using namespace std;
+
 class CMyString
 {
 public:
-	CMyString() {
-
+	CMyString() : m_pszData(nullptr) {
+		cout << "CMyString()" << endl;
 	}
 	~CMyString() {
-
+		cout << "~CMyString()" << endl;
 	}
+private:
+	char* m_pszData;
 };
 
