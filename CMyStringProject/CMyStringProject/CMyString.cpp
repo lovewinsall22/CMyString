8f98105c963146954462e9425245958166bff336
taehyun_0222 <taehyun0222@naver.com>
2026-01-16 08:36:02 +0900

    헤더파일 인클루드, 네임스페이스 cpp 파일로 옮기기 + 생성자,소멸자도 분리

--- CMyStringProject/CMyStringProject/CMyString.cpp
+++ CMyStringProject/CMyStringProject/CMyString.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
 #include "CMyString.h"
 
+using namespace std;
+
+CMyString() : m_pszData(nullptr) {
+	cout << "CMyString()" << endl;
+}
+~CMyString() {
+	cout << "~CMyString()" << endl;
+}
 
 void CMyString::setData(char* pParam)
 {
--- CMyStringProject/CMyStringProject/CMyString.h
+++ CMyStringProject/CMyStringProject/CMyString.h
@@ -1,16 +1,10 @@
 #pragma once
-#include <iostream>
-using namespace std;
 
 class CMyString
 {
 public:
-	CMyString() : m_pszData(nullptr) {
-		cout << "CMyString()" << endl;
-	}
-	~CMyString() {
-		cout << "~CMyString()" << endl;
-	}
+	CMyString()
+		~CMyString();
 
 	char* getData() {
 		return m_pszData;
