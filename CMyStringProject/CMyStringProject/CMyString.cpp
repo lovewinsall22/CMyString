a829e61604d32530172ee8a88be72c67fddea250
taehyun_0222 <taehyun0222@naver.com>
2026-01-16 09:02:10 +0900

    멤버변수 length 추가 후 getLength 함수 추가

--- CMyStringProject/CMyStringProject/CMyString.cpp
+++ CMyStringProject/CMyStringProject/CMyString.cpp
@@ -3,12 +3,17 @@
 
 using namespace std;
 
-CMyString::CMyString() : m_pszData(nullptr) {
+CMyString::CMyString() : m_pszData(nullptr), length(0) {
 	cout << "CMyString()" << endl;
 }
 CMyString::~CMyString() {
 	cout << "~CMyString()" << endl;
 	delete[] m_pszData;
+}
+
+size_t CMyString::getLength()
+{
+	return this->length;
 }
 
 char* CMyString::getData()
@@ -24,4 +29,5 @@ void CMyString::setData(const char* pParam)
 	m_pszData = new char[length + 1];
 
 	strcpy_s(m_pszData, length + 1, pParam);
+	this->length = length;
 }
--- CMyStringProject/CMyStringProject/CMyString.h
+++ CMyStringProject/CMyStringProject/CMyString.h
@@ -6,9 +6,13 @@ public:
 	CMyString();
 	~CMyString();
 
+	size_t getLength();
+
+	char* getData();
 	void setData(const char* pParam);
 
 private:
 	char* m_pszData;
+	size_t length;
 };
 
