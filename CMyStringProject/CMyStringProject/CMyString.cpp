617e499d146ffbd2ec95482550372140c0077b9c
lovewinsall22 <taehyun0222@naver.com>
2026-03-13 18:15:58 +0900

    이동생성자 및 이동대입연산자 선언 및 정의

--- CMyStringProject/CMyStringProject/CMyString.cpp
+++ CMyStringProject/CMyStringProject/CMyString.cpp
@@ -15,6 +15,11 @@ CMyString::CMyString(const CMyString& rhs) {
 	this->setData(rhs.getData()); // 복사생성자의 딮카피 코드 => setData가 내부적으로 동적할당코드
 
 }
+CMyString::CMyString(CMyString&& rhs) noexcept {
+	cout << "CMyString(CMyString&& rhs) - move" << endl;
+	this->m_pszData = rhs.m_pszData; // shallow copy
+	rhs.m_pszData = nullptr;
+}
 CMyString::~CMyString() {
 	cout << "~CMyString()" << endl;
 	delete[] m_pszData;
@@ -24,6 +29,12 @@ void CMyString::operator=(const CMyString& rhs)
 {
 	cout << "CMyString::Operator=(const CMyString& rhs)" << endl;
 	this->setData(rhs.getData());
+}
+void CMyString::operator=(CMyString&& rhs) noexcept
+{
+	cout << "CMyString::Operator=(CMyString&& rhs) - move" << endl;
+	this->m_pszData = rhs.m_pszData; // shallow copy
+	rhs.m_pszData = nullptr;
 }
 
 const size_t CMyString::getLength() const
--- CMyStringProject/CMyStringProject/CMyString.h
+++ CMyStringProject/CMyStringProject/CMyString.h
@@ -6,10 +6,11 @@ public:
 	CMyString();
 	explicit CMyString(const char* param); // 묵시적변환을 막는 변환생성자
 	CMyString(const CMyString& rhs); // 딮카피를 위한 복사생성자
+	CMyString(CMyString&& rhs) noexcept; // 임시객체 상수형 참조를 위한 이동생성자
 	~CMyString();
 
 	void operator=(const CMyString& rhs); // 딮카피를 위한 단순대입연산자 재정의
-
+	void operator=(CMyString&& rhs) noexcept; // 이동생성자의 세트인 이동대입연산자 재정의
 	operator const char* () {
 		return m_pszData;
 	}
