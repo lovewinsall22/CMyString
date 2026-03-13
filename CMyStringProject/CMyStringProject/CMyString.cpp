ab4aadda53e73d5b8a127ae739aef47afe72bc8f
lovewinsall22 <taehyun0222@naver.com>
2026-03-13 13:57:00 +0900

    오랜만에 복습하며 주석 추가

--- CMyStringProject/CMyStringProject/CMyString.cpp
+++ CMyStringProject/CMyStringProject/CMyString.cpp
@@ -23,11 +23,11 @@ const char* CMyString::getData() const
 
 void CMyString::setData(const char* pParam)
 {
-	if (m_pszData != nullptr)
-		delete[] m_pszData;
-	size_t length = strlen(pParam);
-	m_pszData = new char[length + 1];
+	if (m_pszData != nullptr) // 이미 값이 채워져있다면
+		delete[] m_pszData; // 비워줘야함
+	size_t length = strlen(pParam); // 넘어온 값 길이 체크
+	m_pszData = new char[length + 1]; // 널문자까지의 길이로 할당
 
-	strcpy_s(m_pszData, length + 1, pParam);
-	this->length = length;
+	strcpy_s(m_pszData, length + 1, pParam); // 스트링카피
+	this->length = length; // 길이 최신화
 }
