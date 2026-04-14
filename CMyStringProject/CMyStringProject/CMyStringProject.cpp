5b32cf70375ce3798110fab8bc1f2ed895d73380
lovewinsall22 <taehyun0222@naver.com>
2026-04-14 15:24:19 +0900

    메인함수 정리

--- CMyStringProject/CMyStringProject/CMyStringProject.cpp
+++ CMyStringProject/CMyStringProject/CMyStringProject.cpp
@@ -10,25 +10,9 @@ CMyString getHello()
 
 int main()
 {
-    //CMyString result; // 객체생성
-    //result = ::getHello(); // 반환되는 임시객체로 인해 이동대입연산자 실행
-    //std::cout << result << std::endl;
-
-    //CMyString test(std::move(::getHello())); // 이동 생성자로 객체 생성
-
     CMyString hello("hello ");
     CMyString world("world");
 
-    CMyString result(hello + world);
-    std::cout << result << std::endl;
-    std::cout << "bye " + world << std::endl;
-
-    std::cout << "+=+=+=+=+=" << std::endl;
-    result += "!!!!";
-
-    CMyString a("????");
-    result += a;
-    std::cout << result << std::endl;
 
     return 0;
 
