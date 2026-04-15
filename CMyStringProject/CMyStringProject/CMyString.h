83c59e2a4373d9b6a9796a51c973bb5ecb590b61
lovewinsall22 <taehyun0222@naver.com>
2026-04-15 15:40:28 +0900

    소멸자 가상화

--- CMyStringProject/CMyStringProject/CMyString.h
+++ CMyStringProject/CMyStringProject/CMyString.h
@@ -8,7 +8,7 @@ public:
 	explicit CMyString(const char* param); // 묵시적변환을 막는 변환생성자 ===> 객체의생성/소멸로 인한 성능저하를 막고 단순대입이 변환생성자를 호출하는 혼란을 막기 위해 explicit 넣기
 	CMyString(const CMyString& rhs); // 딮카피를 위한 복사생성자 ===> 기본적으로 얕은복사를 하여 댕글링포인터가 발생할 위험을 방지하기 위해 딮카피하는 복사생성자 기술
 	CMyString(CMyString&& rhs) noexcept; // 임시객체 상수형 참조를 위한 이동생성자 ===> 의도적 얕은복사를 하여 성능저하를 막기
-	~CMyString();
+	virtual ~CMyString();
 
 	CMyString& operator=(const CMyString& rhs); // 딮카피를 위한 단순대입연산자 재정의 ===> 재정의 안할시 얕은복사 수행 ... 해제시 댕글링포인터 오류발생
 	CMyString& operator=(CMyString&& rhs) noexcept; // 이동생성자의 세트인 이동대입연산자 재정의 ===> 재정의 안할시 얕은복사만 수행 ... 해제시 댕글링포인터 오류발생
--- CMyStringProject/CMyStringProject/CMyStringProject.cpp
+++ CMyStringProject/CMyStringProject/CMyStringProject.cpp
@@ -12,8 +12,9 @@ CMyString getHello()
 int main()
 {
     CMyStringEx a("BYE");
+    CMyString& b = a;
 
-    std::cout << a << std::endl;
+    std::cout << b << std::endl;
 
     return 0;
 
